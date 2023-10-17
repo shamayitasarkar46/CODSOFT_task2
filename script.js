17fc61956d71549ede53a668189c730ee78638fc
shamayitasarkar46 <145526018+shamayitasarkar46@users.noreply.github.com>
2023-10-17 13:26:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+})
